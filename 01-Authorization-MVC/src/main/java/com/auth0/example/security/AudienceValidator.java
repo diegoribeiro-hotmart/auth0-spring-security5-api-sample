b86fc93cd8cc55fb6ac037c384b9cf542e0add44
Diego Drumond <diego.ribeiro@hotmart.com>
2025-02-14 14:50:55 -0300

    Update AudienceValidator.java

--- 01-Authorization-MVC/src/main/java/com/auth0/example/security/AudienceValidator.java
+++ 01-Authorization-MVC/src/main/java/com/auth0/example/security/AudienceValidator.java
@@ -18,7 +18,7 @@ class AudienceValidator implements OAuth2TokenValidator<Jwt> {
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
         OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
 
-        if (jwt.getAudience().contains(audience)) {
+        if (jwt.getAudience() != null && audience != null && jwt.getAudience().contains(audience)) {
             return OAuth2TokenValidatorResult.success();
         }
 
